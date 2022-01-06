436cbc53e5d019e486d3a77ca7912b3acf714a4d
Pattycakes <contact@patrickgroh.com>
2022-01-06 14:19:22 -0600

    Update clock.v

--- clock.v
+++ clock.v
@@ -1,76 +1,44 @@
 module fpga_top (
-    input      clk,
-    input[5:0] time_set,
-    input      hour_set,
-    input      minute_set,
-    input      clock_on,
-	input      reset,
-	output reg [16:0] leds
+    input clk, 
+    input [5:0] settime,
+    input setampm,
+    input sethour,
+    input setminute,
+    input clockon,
+    input reset,
+    output reg [16:0] leds
 );
-	under_seconds(.clk(clk), .on(clock_on))	
-endmodule
-
-
-module t_flipflop (
-    input clk,
-    input rstn,
-    input t,
-    output reg q
-);
-	always @ (posedge clk) begin
-		if(!rstn)
-			q <= 0;
-		else begin
-			if (t)
-				q <= ~q;
-			else 
-				q <= q;
-		end
-	end 
-endmodule
-
-module under_seconds (
-	input clk,
-	input rstn,
-	input on,
-	output wire second_clk
-);
-	wire [14:0] feedback;
-	t_flipflop(.clk(clk), .rstn(rstn), .t(on), .q(feedback[0]));
-	for (i = 0; i < 14; i = i + 1) begin
-		t_flipflop(.clk(feedback[i]), .rstn(rstn), .t(on), .q(feedback[i+1]));
-	end 
-	t_flipflop(.clk(feedback[14]), .rstn(rstn), .t(on), .q(second_clk));
-endmodule
-
-module flipflop_6wide (
-	input clk,
-	input rstn,
-	input [5:0] timeset,
-	input setting,
-	output wire [5:0] q
-);
-	wire [5:0] feedback;
-	t_flipflop(.clk(clk), .rstn(rstn), .t(1), .q(feedback[0]));
-	t_flipflop(.clk(q[0]), .rstn(rstn), .t(1), .q(feedback[1]));
-	t_flipflop(.clk(q[1]), .rstn(rstn), .t(1), .q(feedback[2]));
-	t_flipflop(.clk(q[2]), .rstn(rstn), .t(1), .q(feedback[3]));
-	t_flipflop(.clk(q[3]), .rstn(rstn), .t(1), .q(feedback[4]));
-	t_flipflop(.clk(q[4]), .rstn(rstn), .t(1), .q(feedback[5]));
-	assign q = setting ? timeset : feedback;
-endmodule
-
-module flipflop_4wide (
-	input clk,
-	input rstn,
-	input [3:0] timeset,
-	input setting,
-	output wire [3:0] q
-);
-	wire [3:0] feedback;
-	t_flipflop(.clk(clk), .rstn(rstn), .t(1), .q(feedback[0]));
-	t_flipflop(.clk(q[0]), .rstn(rstn), .t(1), .q(feedback[1]));
-	t_flipflop(.clk(q[1]), .rstn(rstn), .t(1), .q(feedback[2]));
-	t_flipflop(.clk(q[2]), .rstn(rstn), .t(1), .q(feedback[3]));
-	assign q = setting ? timeset : feedback;
+    reg [31:0] dur;
+    initial begin
+        dur = 0;
+    end
+    always @ (posedge clk) begin
+        if(clockon) begin
+            dur = dur + 1; 
+        end
+        fork //This will lead to the second disappearing before the minute counts up, but that'd only be visible for 1/32768th of a second, so that's fine. 
+            if(dur[20:15] == 60) begin
+                dur[21] = ~dur[21];
+                dur[20:15] = 0;
+            end
+            if(dur[26:21] == 60 && !setminute) begin
+                dur[27] = ~dur[27];
+                dur[26:21] = 0;
+            end
+            if(dur[30:27] == 12 && !sethour) begin
+                dur[31] = ~dur[31];
+                dur[30:27] = 0;
+            end
+            if(setminute) begin
+                dur[26:21] = settime;
+            end
+            if(sethour) begin
+                dur[30:27] = settime[3:0];
+            end
+            if(setampm) begin
+                dur[31] = settime[0];
+            end
+        join
+        leds = dur[31:15];
+    end
 endmodule
